4050f034920f27c973eaf4960f8d1034fb66c344
dudueasy <dudueasy@126.com>
2019-04-11 21:18:28 +0800

    重构代码: 使用 面向对象模型

--- calculator/index.js
+++ calculator/index.js
@@ -1,35 +1,105 @@
 {
-    // 定义计算器整体容器
-    var contanier_1 = document.createElement('div');
-    contanier_1.classList.add('calculator');
-    document.body.appendChild(contanier_1);
-    // 定义计算输出容器
-    var output = document.createElement('div');
-    output.classList.add('output');
-    var span = document.createElement('span');
-    span.textContent = '0';
-    output.appendChild(span);
-    contanier_1.appendChild(output);
+    var Calculator = /** @class */ (function () {
+        function Calculator(keys) {
+            this.keys = keys;
+            this.createContainer();
+            this.createOutput();
+            this.createRow(this.keys);
+            this.bindEvents();
+        }
+        Calculator.prototype.createContainer = function () {
+            this.container = document.createElement('div');
+            this.container.classList.add('calculator');
+            document.body.appendChild(this.container);
+        };
+        Calculator.prototype.createOutput = function () {
+            this.output = document.createElement('div');
+            this.output.classList.add('output');
+            var span = document.createElement('span');
+            span.textContent = '0';
+            this.output.appendChild(span);
+            this.container.appendChild(this.output);
+        };
+        Calculator.prototype.createButton = function (key, rowContainer) {
+            var text = key.text, type = key.type;
+            var className = "button text-" + text + " " + type;
+            var button = document.createElement('button');
+            button.textContent = text;
+            button.className = className;
+            rowContainer.appendChild(button);
+        };
+        Calculator.prototype.createRow = function (keys) {
+            var _this = this;
+            keys.map(function (keyList) {
+                var row = document.createElement('div');
+                row.classList.add('row');
+                keyList.forEach(function (key) { return _this.createButton(key, row); });
+                _this.container.appendChild(row);
+            });
+        };
+        Calculator.prototype.bindEvents = function () {
+            this.container.addEventListener('click', function (e) {
+                if (e.target instanceof HTMLButtonElement) {
+                    var button = e.target;
+                    console.log(button);
+                    console.log(button.classList.value);
+                    if (button.classList.value.indexOf('number') > -1) {
+                        // 如果没有 操作符, 那么拼接数字 firstNumber
+                        // 如果有操作符, 那么拼接数字 secondNumber
+                    }
+                    else if (button.classList.value.indexOf('operator') > -1) {
+                        var operator = button.textContent;
+                        console.log(operator);
+                        switch (operator) {
+                            case ("÷"):
+                                break;
+                            case ("+"):
+                                break;
+                            case ("-"):
+                                break;
+                            case ("×"):
+                                break;
+                            default:
+                                return;
+                        }
+                    }
+                    else if (button.classList.value.indexOf('command') > -1) {
+                        // 将结果作为 firstNumber
+                        // 将操作符清空
+                    }
+                }
+            });
+        };
+        return Calculator;
+    }());
     var keys = [
-        // let keys: Key[] = [
-        ['Clear', '÷'],
-        ['7', '8', '9', '✖',],
-        ['4', '5', '6', '-',],
-        ['1', '2', '3', '+',],
-        ['0', '.', '=',],
+        [
+            { "text": "Clear", type: "command" },
+            { "text": "÷", type: "operator" }
+        ],
+        [
+            { "text": "7", type: "number" },
+            { "text": "8", type: "number" },
+            { "text": "9", type: "number" },
+            { "text": "×", type: "operator" }
+        ],
+        [
+            { "text": "4", type: "number" },
+            { "text": "5", type: "number" },
+            { "text": "6", type: "number" },
+            { "text": "-", type: "operator" }
+        ],
+        [
+            { "text": "1", type: "number" },
+            { "text": "2", type: "number" },
+            { "text": "3", type: "number" },
+            { "text": "+", type: "operator" }
+        ],
+        [
+            { "text": "0", type: "number" },
+            { "text": ".", type: "number" },
+            { "text": "=", type: "command" }
+        ]
     ];
-    function createButton(text, rowContainer, className) {
-        if (className === void 0) { className = ''; }
-        var button = document.createElement('button');
-        button.textContent = text;
-        button.className = className;
-        rowContainer.appendChild(button);
-    }
-    function createRow(keyList) {
-        var row = document.createElement('div');
-        row.classList.add('row');
-        keyList.forEach(function (key) { return createButton(key, row, "button text-" + key); });
-        contanier_1.appendChild(row);
-    }
-    keys.map(function (keyList) { return createRow(keyList); });
+    var calculator = new Calculator(keys);
 }
--- calculator/index.ts
+++ calculator/index.ts
@@ -1,42 +1,146 @@
 {
-  // 定义计算器整体容器
-  let contanier: HTMLDivElement = document.createElement('div')
-  contanier.classList.add('calculator')
-  document.body.appendChild(contanier)
+  interface Key {
+    text: string,
+    type: string
+  }
+
+  class Calculator {
+    public container: HTMLDivElement;
+    public output: HTMLDivElement;
+    public firstNumber: number;
+    public secondNumber: number;
+    public operator: string;
+
+    constructor(public keys: Array<Array<Key>>) {
+      this.createContainer()
+      this.createOutput()
+      this.createRow(this.keys)
+      this.bindEvents()
+    }
+
+    createContainer() {
+      this.container = document.createElement('div')
+      this.container.classList.add('calculator')
+      document.body.appendChild(this.container)
+    }
+
+    createOutput() {
+      this.output = document.createElement('div')
+      this.output.classList.add('output')
+
+      let span: HTMLSpanElement = document.createElement('span')
+      span.textContent = '0'
+      this.output.appendChild(span)
+      this.container.appendChild(this.output)
+    }
+
+    createButton(key: Key, rowContainer: HTMLDivElement): void {
+      const { text, type } = key
+
+      let className = `button text-${text} ${type}`
+      let button: HTMLButtonElement = document.createElement('button')
+      button.textContent = text
+      button.className = className
+      rowContainer.appendChild(button)
+    }
+
+    createRow(keys: Array<Array<Key>>) {
+      keys.map(
+        keyList => {
+          let row: HTMLDivElement = document.createElement('div')
+          row.classList.add('row')
+          keyList.forEach(key => this.createButton(key, row))
+          this.container.appendChild(row)
+        }
+      )
+    }
+
+    bindEvents() {
+      this.container.addEventListener('click', (e: Event) => {
+        if (e.target instanceof HTMLButtonElement) {
+          let button: HTMLButtonElement = e.target
+          console.log(button)
+          console.log(button.classList.value)
+
+          if (button.classList.value.indexOf('number') > -1) {
+            // 如果没有 操作符, 那么拼接数字 firstNumber
+            // 如果有操作符, 那么拼接数字 secondNumber
+
+          }
+          else if (button.classList.value.indexOf('operator') > -1) {
+            let operator: string = button.textContent
+            console.log(operator)
+
+            switch (operator) {
+              case ("÷"):
+                break;
+
+              case ("+"):
+                break;
+
+              case ("-"):
+                break;
+
+              case ("×"):
+                break;
+
+              default:
+                return;
+            }
 
 
-  // 定义计算输出容器
-  let output: HTMLDivElement = document.createElement('div')
-  output.classList.add('output')
+          }
 
-  let span: HTMLSpanElement = document.createElement('span')
-  span.textContent = '0'
-  output.appendChild(span)
-  contanier.appendChild(output)
+          else if (button.classList.value.indexOf('command') > -1) {
+            // 将结果作为 firstNumber
+            // 将操作符清空
+
+          }
 
 
-  let keys: Array<Array<string>> = [
-    // let keys: Key[] = [
-    ['Clear', '÷'],
-    ['7', '8', '9', '✖',],
-    ['4', '5', '6', '-',],
-    ['1', '2', '3', '+',],
-    ['0', '.', '=',],
+
+
+        }
+      })
+    }
+  }
+
+  let keys: Array<Array<Key>> = [
+    [
+      { "text": "Clear", type: "command" },
+      { "text": "÷", type: "operator" }
+    ],
+    [
+      { "text": "7", type: "number" },
+      { "text": "8", type: "number" },
+      { "text": "9", type: "number" },
+      { "text": "×", type: "operator" }
+    ],
+    [
+      { "text": "4", type: "number" },
+      { "text": "5", type: "number" },
+      { "text": "6", type: "number" },
+      { "text": "-", type: "operator" }
+    ],
+    [
+      { "text": "1", type: "number" },
+      { "text": "2", type: "number" },
+      { "text": "3", type: "number" },
+      { "text": "+", type: "operator" }
+    ],
+    [
+      { "text": "0", type: "number" },
+      { "text": ".", type: "number" },
+      { "text": "=", type: "command" }
+    ]
   ]
 
-  function createButton(text: string, rowContainer: HTMLElement, className: string = ''): void {
-    let button: HTMLButtonElement = document.createElement('button')
-    button.textContent = text
-    button.className = className
-    rowContainer.appendChild(button)
-  }
 
-  function createRow(keyList: Array<string>) {
-    let row: HTMLDivElement = document.createElement('div')
-    row.classList.add('row')
-    keyList.forEach(key => createButton(key, row, `button text-${key}`))
-    contanier.appendChild(row)
-  }
-
-  keys.map(keyList => createRow(keyList))
+  let calculator = new Calculator(keys)
 }
+
+
+
+
+
+
