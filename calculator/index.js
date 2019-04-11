465c58b33be5f3aace2288bc3b3416113da5a1c0
dudueasy <dudueasy@126.com>
2019-04-11 19:11:47 +0800

    应用苹果风格的计算器样式

--- calculator/index.js
+++ calculator/index.js
@@ -1,14 +1,22 @@
 {
+    // 定义计算器整体容器
     var contanier_1 = document.createElement('div');
-    contanier_1.classList.add('container');
+    contanier_1.classList.add('calculator');
     document.body.appendChild(contanier_1);
+    // 定义计算输出容器
+    var output = document.createElement('div');
+    output.classList.add('output');
+    var span = document.createElement('span');
+    span.textContent = '0';
+    output.appendChild(span);
+    contanier_1.appendChild(output);
     var keys = [
         // let keys: Key[] = [
-        ['clear', '÷'],
+        ['Clear', '÷'],
         ['7', '8', '9', '✖',],
         ['4', '5', '6', '-',],
         ['1', '2', '3', '+',],
-        ['0', '.', '-',],
+        ['0', '.', '=',],
     ];
     function createButton(text, rowContainer, className) {
         if (className === void 0) { className = ''; }
@@ -20,7 +28,7 @@
     function createRow(keyList) {
         var row = document.createElement('div');
         row.classList.add('row');
-        keyList.forEach(function (key) { return createButton(key, row); });
+        keyList.forEach(function (key) { return createButton(key, row, "button text-" + key); });
         contanier_1.appendChild(row);
     }
     keys.map(function (keyList) { return createRow(keyList); });
--- calculator/index.ts
+++ calculator/index.ts
@@ -1,15 +1,27 @@
 {
+  // 定义计算器整体容器
   let contanier: HTMLDivElement = document.createElement('div')
-  contanier.classList.add('container')
+  contanier.classList.add('calculator')
   document.body.appendChild(contanier)
+
+
+  // 定义计算输出容器
+  let output: HTMLDivElement = document.createElement('div')
+  output.classList.add('output')
+
+  let span: HTMLSpanElement = document.createElement('span')
+  span.textContent = '0'
+  output.appendChild(span)
+  contanier.appendChild(output)
+
 
   let keys: Array<Array<string>> = [
     // let keys: Key[] = [
-    ['clear', '÷'],
+    ['Clear', '÷'],
     ['7', '8', '9', '✖',],
     ['4', '5', '6', '-',],
     ['1', '2', '3', '+',],
-    ['0', '.', '-',],
+    ['0', '.', '=',],
   ]
 
   function createButton(text: string, rowContainer: HTMLElement, className: string = ''): void {
@@ -22,7 +34,7 @@
   function createRow(keyList: Array<string>) {
     let row: HTMLDivElement = document.createElement('div')
     row.classList.add('row')
-    keyList.forEach(key => createButton(key, row))
+    keyList.forEach(key => createButton(key, row, `button text-${key}`))
     contanier.appendChild(row)
   }
 
