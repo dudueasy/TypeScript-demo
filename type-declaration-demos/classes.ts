d2cb8fba153b3802a98d4586467c76539cc8650c
dudueasy <dudueasy@126.com>
2019-04-13 21:18:59 +0800

    添加了ts  类 的示例

--- type-declaration-demos/classes.ts
+++ type-declaration-demos/classes.ts
@@ -1,56 +1,93 @@
-class User{
-  public name: string;
-  protected email: string;
-  public age: number;
+{
+  /* example1  类的定义: 
+  static, private, protected 关键字 
+  getter, setter
+  */
+  class User {
+    private name: string;
+    protected email: string;
+    public _age: number;
 
-  constructor(name: string, email: string, age: number){
-    this.name = name
-    this.email = email
-    this.age = age 
+    // 声明静态成员 (构造函数上的属性)
+    static className = 'User';
+
+    static tellClass() {
+      return (User.className)
+    }
+
+    // 这些函数都是原型的方法
+    constructor(name: string, email: string, _age: number) {
+      this.name = name
+      this.email = email
+      this._age = _age
+    }
+
+    public tell(): void {
+      console.log(this.name + " is " + this.age + " years old")
+    }
+
+    // getter setter
+    get age() {
+      return this._age
+    }
+
+    set age(age) {
+      if (age > 0) {
+        this._age = age
+      }
+      else {
+        throw Error('年龄不能小于0')
+      }
+    }
   }
 
-  public register():void{
-    console.log(this.name + " is now registered")
+  let apolo = new User('apolo', 'xx@11.com', 20)
+  console.log("apolo: ", apolo)
+
+  console.log("User: ", User)
+  console.log("User.className: ", User.className)
+  console.log("User.tellClass(): ", User.tellClass())
+  /* example1 ends */
+
+
+  /* example2  类的继承 */
+  class Member extends User {
+    public id: number
+
+    constructor(name: string, email: string, age: number, id: number) {
+      super(name, email, age)
+      this.id = id
+    }
   }
 
-  public tell():void{
-    console.log(this.name + " is " + this.age + " years old")
+  let alex = new Member('alex', 'xx@1.com', 20, 111)
+
+  // 在 constructor 的参数中使用 public 关键字来快捷定义实例上的字段 
+  class Person {
+    constructor(public name: string, public age: number, public gender: string) {
+    }
   }
 
-  // 这是原型的方法
-  payInvoice():void{
-    console.log(`${this.name} pay invoice`) }
-}
+  let Alex = new Person('Alex', 30, 'male')
+  console.log(Alex)
+  /* example2 ends */
 
-let apolo = new User('apolo', 'xx@11.com', 20)
-apolo.payInvoice()
+  /* example3 实现一个接口 */
+  interface Animal {
+    walk(): void;
+    eat(): void;
+  }
 
+  class Human implements Animal {
+    constructor(public name: string, public age: number) {
+    }
 
-class Member extends User{
-  public id: number
+    walk(): void {
+      console.log('Human is walking')
+    }
 
-  constructor(name: string, email: string, age: number, id: number){
-    super(name, email, age)
-    this.id = id 
+    eat(): void {
+      console.log('Human is eating')
+    }
   } 
-  
-  // 这是原型上的方法
-  payInvoice():void{ 
-    super.payInvoice()
-  }
-} 
-
-let alex = new Member('alex','xx@1.com', 20, 111)
-alex.payInvoice()
-
-
-
-// 在 constructor 的参数中使用 public 关键字来快捷定义实例上的字段
-
-class Person{
-  constructor(public name:string, public age:number, public gender:string){
-  }
 }
-
-let Alex = new Person('Alex', 30, 'male')
-console.log(Alex)
