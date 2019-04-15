cd23aeaa3b8fbc0cddbaf7d53647e5c2d856493a
dudueasy <dudueasy@126.com>
2019-04-16 04:40:18 +0800

    update function demo

--- type-declaration-demos/functions.js
+++ type-declaration-demos/functions.js
@@ -1,12 +1,47 @@
-function getSum(x, y) {
-    return x + y;
-}
-console.log(getSum(1, 4));
-function getName(firstName, lastname) {
-    return lastname ? (firstName + lastname) : firstName;
-}
-console.log(getName('apolo'));
-function returnVoid() {
-    return;
+var _this = this;
+{
+    /* Example1 Basic Function Demo*/
+    function getSum(x, y) {
+        if (x + y > 0)
+            return x + y;
+        else {
+            return NaN;
+        }
+    }
+    console.log(getSum(1, -2));
+    function getName(firstName, lastname) {
+        return lastname ? (firstName + lastname) : firstName;
+    }
+    console.log(getName('apolo'));
+    function returnVoid() {
+        console.log(arguments);
+        return;
+    }
+    // returnVoid(1, 2, 3, 4)
+    // 这样会报错, 因为函数定义中没有参数, 
+    //  所以传入参数是不被允许的
+    // 这样不会报错. 因为 ts 不会对 .call()  的参数进行检查
+    returnVoid.call('whatever', 1, 2, 3, 4);
+    /* Example1 end */
+    /* Example2 arrow Function Demo*/
+    var fn = function (a, b) {
+        // console.log(arguments) 
+        // 在 JS 中,  箭头函数中是不存在 arguments 的
+        // this 由调用方式决定
+        console.log(_this);
+        return a + b;
+    };
+    fn(10, 20);
+    function anotherFn() {
+        var _this = this;
+        var xx = function () {
+            // console.log(arguments)
+            // 在 TypeScript 箭头函数中访问 arguments  是不被允许的
+            // this 由函数调用方式决定
+            console.log(_this);
+        };
+        xx();
+    }
+    anotherFn();
 }
 //# sourceMappingURL=functions.js.map
--- type-declaration-demos/functions.ts
+++ type-declaration-demos/functions.ts
@@ -1,17 +1,61 @@
-function getSum(x: number, y: number): number {
-  return x + y
-}
+{
+  /* Example1 Basic Function Demo*/
+  function getSum(x: number, y: number): number {
+    if (x + y > 0) return x + y
+    else {
+      return NaN
+    }
+  }
+  console.log(getSum(1, -2))
 
 
-console.log(getSum(1, 4))
-
-function getName(firstName: string, lastname?: string): string {
-  return lastname ? (firstName + lastname) :  firstName
-}
-
-console.log(getName('apolo'))
+  function getName(firstName: string, lastname?: string): string {
+    return lastname ? (firstName + lastname) : firstName
+  }
+  console.log(getName('apolo'))
 
 
-function returnVoid():void{
-  return;
+  function returnVoid() {
+    console.log(arguments)
+    return;
+  }
+  // returnVoid(1, 2, 3, 4)
+  // 这样会报错, 因为函数定义中没有参数, 
+  //  所以传入参数是不被允许的
+
+
+  // 这样不会报错. 因为 ts 不会对 .call()  的参数进行检查
+  returnVoid.call('whatever', 1, 2, 3, 4)
+
+  /* Example1 end */
+
+
+  /* Example2 arrow Function Demo*/
+  let fn = (a: number, b: number): number => {
+    // console.log(arguments) 
+    // 在 JS 中,  箭头函数中是不存在 arguments 的
+
+    // this 由调用方式决定
+    console.log(this)
+
+    return a + b
+  }
+
+  fn(10, 20)
+
+
+  function anotherFn() {
+    let xx = () => {
+      // console.log(arguments)
+      // 在 TypeScript 箭头函数中访问 arguments  是不被允许的
+
+
+      // this 由函数调用方式决定
+      console.log(this)
+    }
+
+    xx()
+  }
+
+  anotherFn()
 }
